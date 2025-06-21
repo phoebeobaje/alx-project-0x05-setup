7d302b17e51aa0cccfbdafc5977e8b6d36915d36
phoebeobaje <phoeberobertscareer@gmail.com>
2025-06-21 16:10:46 +0100

    generating image

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,14 +1,20 @@
-import * as React from "react"
+import { ReactNode } from "react";
 
 export interface ReactComponentProps {
-    children: React.ReactNode
-}
-export interface GeneratedImageProps{
-    imageUrl:string
-    prompt:string
-    witdth?:string
-    height?:string
-    action:(imagePath:string)=>void
+  children: ReactNode
 }
 
-export type ImageProps=Pick<GeneratedImageProps, "imageUrl" |"prompt">
+
+export interface GeneratedImageProps {
+  imageUrl: string
+  prompt: string
+  width?: string
+  height?: string
+  action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+  prompt: string;
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
